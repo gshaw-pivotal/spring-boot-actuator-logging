12bea109a0ec97b34b0bc2088ba365309b13007a
gshaw-pivotal <gshaw+github@pivotal.io>
2020-01-24 14:35:14 -0500

    Insert a traceFilter immediately after the SecurityContextPersistenceFilter
    - This allows the httptrace on actuator to capture requests that are
      denied by spring security due to invalid credentials.
    - An interesting 'downside' to placing the tracefilter here is that for
      requests that are ultimately successfully authenticated there are 2
      records. Initially the request gets a 401 responses, followed afterwards
      by a second request / response that returns 200 (assuming valid
      credentials).
    - It appears that requests come in and pass the trace filter (and get
      401'd) so they are recorded, followed immediately afterwards by the
      request passing the tracefilter again and ultimately returning a 200.

--- src/main/java/gs/al/actuatorlogging/SecurityConfig.java
+++ src/main/java/gs/al/actuatorlogging/SecurityConfig.java
@@ -1,18 +1,27 @@
 package gs.al.actuatorlogging;
 
+import org.springframework.boot.actuate.web.trace.servlet.HttpTraceFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private HttpTraceFilter traceFilter;
+
+    public SecurityConfig(HttpTraceFilter traceFilter) {
+        this.traceFilter = traceFilter;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .addFilterAfter(traceFilter, SecurityContextPersistenceFilter.class)
                 .httpBasic()
                 .and()
                 .authorizeRequests()
